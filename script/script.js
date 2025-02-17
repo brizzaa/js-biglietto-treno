1d46520f2bf82fa7d36ab6493938161d0cae302c
brizzaa <brizzantex@gmail.com>
2025-02-17 14:30:12 +0100

    feat:commit

--- script/script.js
+++ script/script.js
@@ -1,29 +1,29 @@
 // devo chiedere all'utente anni e km da percorrere
 let UserAnni = prompt("quanti anni hai?");
 let UserKm = prompt("quanti km devi percorrere?");
-const CostoBiglietto = 0.21 * UserKm;
 //controllo e se un valore non è un numero oppure non c'e e dico di riprovare
 if (isNaN(UserAnni) || isNaN(UserKm) || (UserKm == "" || UserAnni == "")){
     alert("hai inserito un valore sbagliato,riprova");
 }
  else {
-    // converto in numeri poichè i numeri dati da prompt sono una stringa
+        // converto in numeri poichè i numeri dati da prompt sono una stringa
     parseInt(UserAnni);
     parseInt(UserKm);
-    // se utente ha meno di 18 anni applico uno sconto del 20% 
+        // se utente ha meno di 18 anni applico uno sconto del 20% 
     if (UserAnni < 18){
-        let ScontoMinorenni =  ((CostoBiglietto * 20) / 100);
-        let PrezzoMinorenni = parseFloat(CostoBiglietto - ScontoMinorenni);
-        alert(`il prezzo è di ${PrezzoMinorenni}€`);
+
+     let CostoBiglietto = parseFloat(((0.21 * UserKm) - ((0.21 * UserKm) * 20 / 100)));
+     alert(`il prezzo è di ${CostoBiglietto}€`);
     }
-    // se utente ha piu di 65 anni applico sconto del 40% 
+        // se utente ha piu di 65 anni applico sconto del 40% 
     else if (UserAnni > 65){
-        let ScontoAnziani = ((CostoBiglietto * 40) / 100);
-        let PrezzoAnziani = parseFloat(CostoBiglietto - ScontoAnziani);
-        alert(`il prezzo è di ${PrezzoAnziani}€`);
+        // calcolo sconto
+        let CostoBiglietto = parseFloat(((0.21 * UserKm) - ((0.21 * UserKm) * 40 / 100)));
+        alert(`il prezzo è di ${CostoBiglietto}€`);
     }
     // se non ha meno di 18 e piu di 65 do il prezzo senza sconto
     else {
-        alert(`il prezzo è di ${parseFloat(CostoBiglietto)}€`);
+        alert(`il prezzo è di ${parseFloat(0.21 * UserKm)}€`);
     }
 } 
+
